2a692c84439e42191a1dca2cc07a6b6da36a8af7
rahowa <roninfromsvf@gmail.com>
2020-04-01 17:20:14 +0300

    add new types

--- src/base_types.py
+++ src/base_types.py
@@ -1,7 +1,10 @@
 import numpy as np
-from typing import Union
+from typing import Union, Tuple, List
 from nptyping import Array
 from albumentations import BasicIAATransform, BasicTransform
 
 Image = Array[np.uint8]
 BaseAug = Union[BasicTransform, BasicIAATransform]
+Labels = Array[int]
+Box = Tuple[float, float, float, float, float]
+Boxes = List[Box]
