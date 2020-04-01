47f3c20c267c02953ffbb58adf75d07ff70bc489
rahowa <roninfromsvf@gmail.com>
2020-04-01 18:04:15 +0300

    add method to extract hdf5 files

--- src/hdf5_utils.py
+++ src/hdf5_utils.py
@@ -8,7 +8,8 @@ import multiprocessing as mp
 import cv2
 
 from src.base_types import Labels, Image
-from src.files_utils import  images
+from src.files_utils import images, create_folders
+
 
 def read_labels_npy(path: str) -> Labels:
     return np.load(path).reshape(-1, 1)
@@ -47,7 +48,7 @@ def save_to_hdf5(result_name: str,
         if labels is None:
             for img_name, img in zip(img_names, img_data):
                 compressed_data = hf.create_dataset(
-                    name=f"data_{img_name.split('.')[0]}",
+                    name=f"data_{img_name}",
                     data=img,
                     shape=(img.shape[0], img.shape[1], img.shape[2]),
                     maxshape=(img.shape[0], img.shape[1], img.shape[2]),
@@ -57,7 +58,7 @@ def save_to_hdf5(result_name: str,
         else:
             for img_name, img, label in zip(img_names, img_data, labels):
                 compressed_data = hf.create_dataset(
-                    name=f"data_{img_name.split('.')[0]}",
+                    name=f"data_{img_name}",
                     data=img,
                     shape=(img.shape[0], img.shape[1], img.shape[2]),
                     maxshape=(img.shape[0], img.shape[1], img.shape[2]),
@@ -65,21 +66,35 @@ def save_to_hdf5(result_name: str,
                     compression_opts=9
                 )
                 compressed_labels = hf.create_dataset(
-                    name=f"label_{img_name}",
+                    name=f"label_{img_name.split('.')[0]}",
                     data=label,
                     shape=(1, ),
                     maxshape=(None, ),
-                    compressin='gzip',
+                    compression='gzip',
                     compression_opts=9
                 )
 
 
-def extract_from_hdft(path: str) -> None:
+def extract_from_hdf5(path: str) -> None:
+    filename = os.path.basename(path)
     with h5py.File(path, 'r') as hf:
+        all_instances = tuple(hf.keys())
+        labels = tuple(filter(lambda x: x.split('_')[0] == 'label', all_instances))
+        data = tuple(filter(lambda x: x.split('_')[0] == 'data', all_instances))
+        create_folders((f'data_{filename.replace(".", "_")}', f'labels_{filename.replace(".", "_")}'))
+        for image in data:
+            cv2.imwrite(os.path.join(f'data_{filename.replace(".", "_")}', f'{image}'), np.array(hf[image]))
+
+        if len(labels) == 0:
+            return
+
+        with open(f'labels_{filename.replace(".", "_")}/labels.txt', 'w') as labels_file:
+            for label in labels:
+                labels_file.write(f'{label}: {str(np.array(hf[label]))}')
+                labels_file.write('\n')
 
 
-
-def main_save_to_hdf5(imdir: str, labels: str = None) -> None:
+def main_save_to_hdf5(imdir: str, labels: str) -> None:
     img_names = images(imdir)
     img_locations = tuple(os.path.join(imdir, img_name) for img_name in img_names)
     img_data = load_images(img_locations)
@@ -87,7 +102,12 @@ def main_save_to_hdf5(imdir: str, labels: str = None) -> None:
         try:
             labels_file = load_labels(labels)
         except NotImplementedError:
+            labels_file = None
             typer.echo("Labels cant be loaded only from .CSV, .TXT or .NPY files\n")
     else:
         labels_file = None
     save_to_hdf5(imdir, img_names, img_data, labels_file)
+
+
+def main_extract_from_hdf5(file: str):
+    extract_from_hdf5(file)
