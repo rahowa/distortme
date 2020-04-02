cf697928cc06628edcc365cc102c336b0cda5b25
rahowa <roninfromsvf@gmail.com>
2020-04-02 18:22:41 +0300

    add new type and fix Image type

--- src/base_types.py
+++ src/base_types.py
@@ -3,7 +3,8 @@ from typing import Union, Tuple, List
 from nptyping import Array
 from albumentations import BasicIAATransform, BasicTransform
 
-Image = Array[np.uint8]
+Image = Array[Union[np.uint8, np.float]]
+ImageShape = Union[Tuple[int, int], Tuple[int, int, int]]
 BaseAug = Union[BasicTransform, BasicIAATransform]
 Labels = Array[int]
 Box = Tuple[float, float, float, float, float]
