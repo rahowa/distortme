7a17476ed168f751cfa8301058f833867f7b1aea
rahowa <roninfromsvf@gmail.com>
2020-04-12 17:17:35 +0300

    add available nn models

---
+++ src/nn_models.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from frozendict import frozendict
+from src.nn_algorithms.faces.wrappers.blaze_face_wrapper import BlazeFaceWrapper
+
+
+class Models(str, Enum):
+    faces = "faces"
+    objects = "objects"
+    classes = "classes"
+    masks = "masks"
+
+
+TASK_TO_MODEL = frozendict(
+    faces=BlazeFaceWrapper
+)
+
