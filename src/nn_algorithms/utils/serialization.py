c90977b3fb7dc7d56871eec13d75b62077bbbe1f
rahowa <roninfromsvf@gmail.com>
2020-04-12 17:16:49 +0300

    add base env to work with nn algorithms

---
+++ src/nn_algorithms/utils/serialization.py
@@ -0,0 +1,10 @@
+import json
+from typing import Sequence
+
+from src.base_types import BaseResult
+
+
+def save_results(result_file: str, files: Sequence[str], results: Sequence[BaseResult]) -> None:
+    result_dict = {file: res.to_dict(file) for file, res in zip(files, results)}
+    with open(result_file, 'w') as res_json: 
+        json.dump(result_dict, res_json)
