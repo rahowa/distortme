931d8ab4a5b4f4d039b9c43a4bdd79f9ed093948
rahowa <roninfromsvf@gmail.com>
2020-06-13 19:12:09 +0300

    add fromrle imaplementation and redactoring

--- src/rle_utils.py
+++ src/rle_utils.py
@@ -1,22 +1,24 @@
 import os
-from typing import Sequence, Tuple
+from typing import Any, Sequence, Tuple, Callable
 
 import cv2
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
 
-from src.base_types import Image, ImageShape
+from src.base_types import Image
 from src.files_utils import images
 from src.main_utils import print_delimiter
 
 
-def rle2mask(mask_rle: str, shape: ImageShape) -> Image:
+def rle2mask(mask_and_shape: Tuple[str, Image]) -> Image:
+    mask_rle, shape = *mask_and_shape
     if mask_rle != mask_rle:
         return np.zeros_like(shape)
 
     s = mask_rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts, lengths = [np.asarray(x, dtype=int)
+                       for x in (s[0:][::2], s[1:][::2])]
     starts -= 1
     ends = starts + lengths
     img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
@@ -34,25 +36,40 @@ def mask2rle(image: Image) -> str:
     return ' '.join(str(x) for x in runs)
 
 
-def convert_to_rle(img_data: Sequence[Image]) -> Tuple[str, ...]:
-    if len(img_data) < 100:
-        return tuple(mask2rle(img) for img in img_data)
+def save_mask(image_data: Tuple[str, Image]) -> None:
+    cv2.imwrite(image_data[0], image_data[1])
+
+
+def proces_async(data: Sequence[Any],
+                 function: Callable[[Tuple[Any, ...]], Any]) -> Tuple[Any, ...]:
+    if len(data) < 100:
+        return tuple(function(data_sample) for data_sample in data)
 
     pool = mp.Pool(mp.cpu_count())
-    return tuple(pool.map(mask2rle, img_data))
+    return tuple(pool.map(function, data))
 
 
-@print_delimiter("Convert masks to rle...")
+@print_delimiter("Convert masks to rle's...")
 def main_torle(imdir: str) -> None:
     img_names = images(imdir)
     img_locations = (os.path.join(imdir, img) for img in img_names)
     img_data = tuple(cv2.imread(img) for img in img_locations)
     img_sizes = (img.shape[:2] for img in img_data)
-    rles = convert_to_rle(img_data)
-    rle_result = pd.DataFrame(tuple(zip(img_names, rles, img_sizes)), columns=["image", 'rle', 'shape'])
+    rles = proces_async(img_data, mask2rle)
+    rle_result = pd.DataFrame(tuple(zip(img_names, rles, img_sizes)),
+                              columns=["image_name", 'rle', 'size'])
     rle_result.to_csv(f"rle_of_{imdir}.csv", index=False)
 
 
-@print_delimiter
-def main_frommrle(filename: str) -> None:
-    pass
+@print_delimiter("Convert rle's to masks...")
+def main_frommrle(filename: str,
+                  rle_column: str,
+                  size_column: str,
+                  name_column: str) -> None:
+    data = pd.read_csv(filename)
+    rles = data[rle_column].value.tolist()
+    shapes = data[size_column].value.tolist()
+    images_names = data[name_column].value.tolist()
+    rle_and_shapes = tuple((rle, shape) for rle, shape in zip(rles, shapes))
+    images = proces_async(rle_and_shapes, rle2mask)
+    proces_async(zip(images_names, images), save_mask)
