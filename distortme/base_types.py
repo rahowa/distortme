b5f0daa7f5145a93d871e12b05cc694aeaf87a11
rahowa <roninfromsvf@gmail.com>
2020-07-09 23:43:09 +0300

    fix array type

--- distortme/base_types.py
+++ distortme/base_types.py
@@ -1,16 +1,16 @@
 import os
 import numpy as np
-from nptyping import Array
+from nptyping import NDArray
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from typing import Union, Tuple, Dict, Any, List
 from albumentations import BasicIAATransform, BasicTransform
 
 
-Image = Array[Union[np.uint8, np.float]]
+Image = NDArray[Union[np.uint8, np.float]]
 ImageShape = Union[Tuple[int, int], Tuple[int, int, int]]
 BaseAug = Union[BasicTransform, BasicIAATransform]
-Labels = Array[int]
+Labels = NDArray[int]
 Box = Tuple[float, float, float, float]
 Boxes = Tuple[Box, ...]
 
@@ -35,13 +35,13 @@ class FaceResult(BaseResult):
         boxes: Boxes, Tuple[float, float, float, float]
             List of detected boxes in format of normalized xmin, ymin, xmax, ymax
         
-        landmarks: List[Array[float, 6]], Sequene[np.ndarray]
+        landmarks: List[NDArray[float, 6]], Sequene[np.ndarray]
             List of detected face landmarks corresponding to boxes
     
     """
     conf: List[float] = field(default_factory=list)
     boxes: Boxes = field(default_factory=tuple)
-    landmarks: List[Array[float, 6]] = field(default_factory=list)
+    landmarks: List[NDArray[float, 6]] = field(default_factory=list)
 
     def to_dict(self, path: str) -> Dict[str, Any]:
         """
@@ -158,10 +158,10 @@ class SegmentationResult(BaseResult):
 
     Parameters
     ----------
-        mask: Array[float]
+        mask: NDArray[float]
     """
 
-    mask: Array[float]
+    mask: NDArray[float]
 
     def to_dict(self, path: str) -> Dict[str, Any]:
         """
